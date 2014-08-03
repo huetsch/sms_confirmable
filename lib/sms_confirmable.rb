fbe61598e72f769069050313816878c12a6da758
Mark Huetsch <markhuetsch@gmail.com>
2014-08-03 16:51:54 +0800

    just pull in the model, dont try to pull in the controller

--- lib/sms_confirmable.rb
+++ lib/sms_confirmable.rb
@@ -14,6 +14,7 @@ end
 module SmsConfirmable
 end
 
-Devise.add_module :sms_confirmable, :model => 'sms_confirmable/models/sms_confirmable', :controller => :confirmable, :route => :confirmable
+#Devise.add_module :sms_confirmable, :model => 'sms_confirmable/models/sms_confirmable', :controller => :confirmable, :route => :confirmable
+Devise.add_module :sms_confirmable, :model => 'sms_confirmable/models/sms_confirmable'
 
 require 'sms_confirmable/models/sms_confirmable'
